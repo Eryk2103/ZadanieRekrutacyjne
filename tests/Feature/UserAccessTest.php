f539362d8cff135c6ed699c1d2c33a2a35ecf930
Eryk Ciesielski <eryk0399@gmail.com>
2023-03-16 23:02:34 +0100

    add tests to check if user can access api routes

---
+++ tests/Feature/UserAccessTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Customer;
+use Tests\TestCase;
+use App\Models\User;
+use Mockery\MockInterface;
+
+class UserAccessTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_user_can_access_get_all(): void
+    {
+        $user = User::factory()->create();
+       
+        $response = $this->actingAs($user)
+            ->get('/api/customers');
+        $response->assertStatus(200);
+    }
+    public function test_user_can_access_get_by_id()
+    {
+        $user = User::factory()->create();
+        $customer = Customer::factory()->create(['user_id' => $user->id]);
+            
+        $response = $this->actingAs($user)
+            ->get('/api/customers/' . $customer->id);
+        $response->assertStatus(200);
+    }
+    public function test_user_can_access_create()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)
+            ->post('/api/customers', [ 
+                'first_name' => 'test',
+                'last_name' => 'test',
+                'phone_number' => 'test',
+                'user_id' => $user->id
+            ]);
+        $response->assertStatus(201);
+    }
+    public function test_user_can_access_update()
+    {
+        $user = User::factory()->create();
+        $customer = Customer::factory()->create(['user_id' => $user->id]);
+
+
+        $response = $this->actingAs($user)
+            ->put('/api/customers/' . $customer->id, [ 
+                'first_name' => 'test2',
+                'last_name' => 'test',
+                'phone_number' => 'test',
+            ]);
+        $response->assertStatus(200);
+    }
+    public function test_user_can_access_delete()
+    {
+        $user = User::factory()->create();
+        $customer = Customer::factory()->create(['user_id' => $user->id]);
+
+
+        $response = $this->actingAs($user)
+            ->delete('/api/customers/' . $customer->id);
+        $response->assertStatus(200);
+    }
+
+}
