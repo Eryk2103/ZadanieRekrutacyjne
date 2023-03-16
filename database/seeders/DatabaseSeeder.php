5c2bd3acbb6af5a31c08b788f9fdd55b5e746b6a
Eryk Ciesielski <eryk0399@gmail.com>
2023-03-16 23:13:34 +0100

    add role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ namespace Database\Seeders;
 use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,13 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //roles
+        $this->call(LaratrustSeeder::class);
+        
         //fake data
         User::factory(10)->create()->each(function ($user) {
            Customer::factory(20)->create(['user_id' => $user->id]);
+           $user->attachRole('user');
         });
+
+        User::factory(2)->create()->each(function ($user) {
+            $user->attachRole('admin');
+         });
 
         //real data
         //$this->call(CustomersTableSeeder::class);
+        
         
     }
 }
