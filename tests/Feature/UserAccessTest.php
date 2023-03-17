d469aabf40d685785615d434568c39a4e8e5cbf9
Eryk Ciesielski <eryk0399@gmail.com>
2023-03-17 11:58:47 +0100

    update tests for roles

--- tests/Feature/UserAccessTest.php
+++ tests/Feature/UserAccessTest.php
@@ -16,7 +16,7 @@ class UserAccessTest extends TestCase
      */
     public function test_user_can_access_get_all(): void
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create()->attachRole('user');
        
         $response = $this->actingAs($user)
             ->get('/api/customers');
@@ -24,16 +24,16 @@ class UserAccessTest extends TestCase
     }
     public function test_user_can_access_get_by_id()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create()->attachRole('user');
         $customer = Customer::factory()->create(['user_id' => $user->id]);
             
         $response = $this->actingAs($user)
             ->get('/api/customers/' . $customer->id);
         $response->assertStatus(200);
     }
-    public function test_user_can_access_create()
+    public function test_admin_can_access_create()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create()->attachRole('admin');
         $response = $this->actingAs($user)
             ->post('/api/customers', [ 
                 'first_name' => 'test',
@@ -45,7 +45,7 @@ class UserAccessTest extends TestCase
     }
     public function test_user_can_access_update()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create()->attachRole('user');
         $customer = Customer::factory()->create(['user_id' => $user->id]);
 
 
@@ -59,13 +59,69 @@ class UserAccessTest extends TestCase
     }
     public function test_user_can_access_delete()
     {
-        $user = User::factory()->create();
+
+        $user = User::factory()->create()->attachRole('user');
         $customer = Customer::factory()->create(['user_id' => $user->id]);
 
 
         $response = $this->actingAs($user)
             ->delete('/api/customers/' . $customer->id);
         $response->assertStatus(200);
+    }
+    //test unauthorized
+    public function test_admin_unauthorized_access_get_all(): void
+    {
+        $user = User::factory()->create()->attachRole('admin');
+       
+        $response = $this->actingAs($user)
+            ->get('/api/customers');
+        $response->assertStatus(403);
+    }
+    public function test_admin_unauthorized_access_get_by_id()
+    {
+        $user = User::factory()->create()->attachRole('admin');
+        $customer = Customer::factory()->create(['user_id' => $user->id]);
+            
+        $response = $this->actingAs($user)
+            ->get('/api/customers/' . $customer->id);
+        $response->assertStatus(403);
+    }
+    public function test_user_unauthorized_access_create()
+    {
+        $user = User::factory()->create()->attachRole('user');
+        $response = $this->actingAs($user)
+            ->post('/api/customers', [ 
+                'first_name' => 'test',
+                'last_name' => 'test',
+                'phone_number' => 'test',
+                'user_id' => $user->id
+            ]);
+        $response->assertStatus(403);
+    }
+    public function test_admin_unauthorized_access_update()
+    {
+        $user = User::factory()->create()->attachRole('admin');
+        $customer = Customer::factory()->create(['user_id' => $user->id]);
+
+
+        $response = $this->actingAs($user)
+            ->put('/api/customers/' . $customer->id, [ 
+                'first_name' => 'test2',
+                'last_name' => 'test',
+                'phone_number' => 'test',
+            ]);
+        $response->assertStatus(403);
+    }
+    public function test_admin_unauthorized_access_delete()
+    {
+
+        $user = User::factory()->create()->attachRole('admin');
+        $customer = Customer::factory()->create(['user_id' => $user->id]);
+
+
+        $response = $this->actingAs($user)
+            ->delete('/api/customers/' . $customer->id);
+        $response->assertStatus(403);
     }
 
 }
